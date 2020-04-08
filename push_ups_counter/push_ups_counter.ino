f1a4c20eac8610c3e8dd29bcbcdbb8bfa9ab6c4b
Rafa <rafa.lozano.rolon@gmail.com>
2020-04-07 19:27:01 -0500

    Funciones principales *Se agrego buzzer para indicar cada rep *Se agrego una señal sonora con el buzzer al iniciar el ejercicio *Sonido cuando finalizo el ejercicio *Temporizador mostrado en segundos y ms

--- push_ups_counter/push_ups_counter.ino
+++ push_ups_counter/push_ups_counter.ino
@@ -1,9 +1,19 @@
 #include <LiquidCrystal_I2C.h>
 
-#define Btn_mas A0
-#define Btn_menos A1
-#define Btn_set A2
-#define Sensor A3
+#define Btn_set A0
+#define Sensor A1
+#define Buzzer A2
+
+const uint16_t objetivo = 10;
+byte flecha[] = {
+    B00000,
+    B00100,
+    B00110,
+    B11111,
+    B11111,
+    B00110,
+    B00100,
+    B00000};
 
 LiquidCrystal_I2C lcd(0x27,20,4);
 
@@ -11,17 +21,110 @@ void setup()
 {
     lcd.init();
     lcd.backlight();
+    lcd.createChar(0, flecha);
+
     lcd.setCursor(0,0);
-    lcd.print("contador push-ups");
+    lcd.print("      PUSH-UPS");
+
+    lcd.setCursor(0, 1);
+    lcd.write(0);
+    lcd.print("Objetivo:");
+    lcd.print(objetivo);
 
     //inicializar pines
-    pinMode(Btn_mas, INPUTPULLUP);
-    pinMode(Btn_menos, INPUTPULLUP);
-    pinMode(Btn_set, INPUTPULLUP);
+    
+    pinMode(Sensor, INPUT_PULLUP);
+    pinMode(Btn_set, INPUT_PULLUP);
+    pinMode(Buzzer, OUTPUT);
 
+    digitalWrite(Buzzer,HIGH);
+
+    Serial.begin(9600);
 }
 
 void loop()
 {
+    double Tiempo_inicial = 0;
+    double Tiempo_activo = 0;
+    int Contador = 0;
+    if (!digitalRead(Btn_set))
+    {
+        
+        while (!digitalRead(Btn_set))
+        {
+            delay(20);
+        }
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("     GET READY  ");
+        lcd.setCursor(9,1);
+        lcd.print("3");
+        digitalWrite(Buzzer,LOW);
+        delay(700);
+        digitalWrite(Buzzer,HIGH);
+        delay(400);
+
+        lcd.setCursor(9, 1);
+        lcd.print("2");
+        digitalWrite(Buzzer, LOW);
+        delay(700);
+        digitalWrite(Buzzer, HIGH);
+        delay(400);
+
+        lcd.setCursor(9, 1);
+        lcd.print("1");
+        digitalWrite(Buzzer, LOW);
+        delay(700);
+        digitalWrite(Buzzer, HIGH);
+        
+        lcd.clear();
+        lcd.print("      PUSH-UPS");
+
+        lcd.setCursor(0, 1);
+        lcd.write(0);
+        lcd.print("Objetivo:");
+        lcd.print(objetivo);
+
+        Tiempo_inicial = millis();
+        while (Contador < objetivo)
+        {
+            Serial.println(Tiempo_activo);
+            Tiempo_activo = (millis() - Tiempo_inicial)/1000;
+            
+
+            lcd.setCursor(0, 3);
+            lcd.write(0);
+            lcd.print("Tiempo:");
+            lcd.print(Tiempo_activo);
+            lcd.print(" s");
+
+            if(!digitalRead(Sensor))
+            {
+                while (!digitalRead(Sensor))
+                {
+                    delay(20);
+                }
+                digitalWrite(Buzzer, LOW);
+                delay(200);
+                digitalWrite(Buzzer, HIGH);
+                Contador++;
+            }
+            lcd.setCursor(0, 2);
+            lcd.write(0);
+            lcd.print("Contador:");
+            lcd.print(Contador);
+        }
+        delay(700);
+        for (size_t i = 0; i < 3; i++)
+        {
+            digitalWrite(Buzzer, LOW);
+            delay(100);
+            digitalWrite(Buzzer, HIGH);
+            delay(100);
+        }
+        
+        
+    }
+
     
 }
